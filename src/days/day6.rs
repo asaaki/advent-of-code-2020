f5e80453a4c2f7f2181120c611804a5b06d6ad3a
asaaki <446613+asaaki@users.noreply.github.com>
2020-12-06 07:37:44 +0100

    Avoid intermediate values/results
    All .collect()'s allocate something new, today we don't do that.

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -10,17 +10,12 @@ pub(crate) fn run_test(step: Step, input: &Vec<String>, expected: String) -> Nul
 const A_IDX: u8 = 'a' as u8;
 
 pub(crate) fn run(step: Step, input: &Vec<String>) -> CustomResult<String> {
-    // a bit unhappy about the owning, but easier than fidlling with lifetimes
-    let groups: Vec<String> = input
-        .join("\n")
-        .split("\n\n")
-        .map(std::borrow::ToOwned::to_owned)
-        .collect();
-
     match step {
         Step::One => {
-            let records: Vec<char> = groups
-                .iter()
+            // we duplicate the initial join/split to avoid a temporary collection
+            let result = input
+                .join("\n")
+                .split("\n\n")
                 .map(|g| {
                     // collect all answers into a single stream of chars
                     let mut chars: Vec<char> = g.replace('\n', "").chars().collect();
@@ -32,18 +27,19 @@ pub(crate) fn run(step: Step, input: &Vec<String>) -> CustomResult<String> {
                     chars
                 })
                 .flatten()
-                .collect();
-            let result: String = format!("{}", records.len());
-            println!("Inputs: groups={}", groups.len());
+                .count();
+            let result: String = format!("{}", result);
             println!("Result = {}", result);
             Ok(result)
         }
 
         Step::Two => {
-            let records: Vec<usize> = groups
-                .iter()
-                .map(|g| {
-                    let answers: Vec<&str> = g.split('\n').collect();
+            let result: usize = input
+                .join("\n")
+                .split("\n\n")
+                .map(|group| {
+                    // split the single string into its newline separated parts
+                    let answers: Vec<&str> = group.split('\n').collect();
                     if answers.len() == 1 {
                         // single person answer's are much simpler to calculate
                         answers[0].len() as usize
@@ -51,21 +47,19 @@ pub(crate) fn run(step: Step, input: &Vec<String>) -> CustomResult<String> {
                         // monstrosity of mapping, folding, bitshifting, bitwise-and-ing, and counting
                         answers
                             .iter()
-                            .map(|&a| {
+                            .map(|&answer|
                                 // step 1: store each person's answer as an u32
                                 // (each letter is a bit, starting from zero, therefore the "minus 'a'")
-                                a.chars()
-                                    .fold(0u32, |acc, c| acc | 1 << ((c as u8) - A_IDX))
-                            })
+                                answer.chars()
+                                    .fold(0u32, |acc, c| acc | 1 << ((c as u8) - A_IDX)))
                             // step 2: bitwise-AND all answers, so only bits common to all of them remain
                             .fold(u32::MAX, |acc, b| acc & b)
                             // step 3: count the 1 bits --- Thank you, Rust, for having that in the stdlib!
                             .count_ones() as usize
                     }
                 })
-                .collect();
-            let result: String = format!("{}", records.iter().sum::<usize>());
-            println!("Inputs: groups={}", groups.len());
+                .sum::<usize>();
+            let result: String = format!("{}", result);
             println!("Result = {}", result);
             Ok(result)
         }
