4462f707cc70df812f8a350817c74a553ca16080
asaaki <446613+asaaki@users.noreply.github.com>
2020-12-06 07:55:31 +0100

    Clean up a bit
    Prefer u32 as it is the smallest possible integer size we need.

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -7,6 +7,7 @@ pub(crate) fn run_test(step: Step, input: &Vec<String>, expected: String) -> Nul
     Ok(())
 }
 
+const MAX_ANSWERS: u32 = 0b11111111111111111111111111; // 26 bit (a-z)
 const A_IDX: u8 = 'a' as u8;
 
 pub(crate) fn run(step: Step, input: &Vec<String>) -> CustomResult<String> {
@@ -34,7 +35,7 @@ pub(crate) fn run(step: Step, input: &Vec<String>) -> CustomResult<String> {
         }
 
         Step::Two => {
-            let result: usize = input
+            let result: u32 = input
                 .join("\n")
                 .split("\n\n")
                 .map(|group| {
@@ -42,23 +43,25 @@ pub(crate) fn run(step: Step, input: &Vec<String>) -> CustomResult<String> {
                     let answers: Vec<&str> = group.split('\n').collect();
                     if answers.len() == 1 {
                         // single person answer's are much simpler to calculate
-                        answers[0].len() as usize
+                        answers[0].len() as u32
                     } else {
-                        // monstrosity of mapping, folding, bitshifting, bitwise-and-ing, and counting
+                        // monstrosity of mapping, folding, bitshifting, bitwise-and-ing, & counting
                         answers
                             .iter()
                             .map(|&answer|
-                                // step 1: store each person's answer as an u32
-                                // (each letter is a bit, starting from zero, therefore the "minus 'a'")
+                                // step 1: store each person's answer as an u32, to fit max 26
+                                // questions; each letter is a bit, starting from zero,
+                                // therefore the "minus 'a'" (A_IDX) before shifting
                                 answer.chars()
+                                    // note: casting chars to u8 is safe, since we're dealing with ASCII only
                                     .fold(0u32, |acc, c| acc | 1 << ((c as u8) - A_IDX)))
                             // step 2: bitwise-AND all answers, so only bits common to all of them remain
-                            .fold(u32::MAX, |acc, b| acc & b)
+                            .fold(MAX_ANSWERS, |acc, b| acc & b)
                             // step 3: count the 1 bits --- Thank you, Rust, for having that in the stdlib!
-                            .count_ones() as usize
+                            .count_ones()
                     }
                 })
-                .sum::<usize>();
+                .sum();
             let result: String = format!("{}", result);
             println!("Result = {}", result);
             Ok(result)
